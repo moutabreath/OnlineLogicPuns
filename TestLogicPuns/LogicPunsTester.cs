630f5d4e224ee60e9e595ddcd46eebfcd3a466e5
Tal Druckmann <tdruck@gmail.com>
2021-08-26 12:16:12 +0300

    Added some more logic puns

--- TestLogicPuns/LogicPunsTester.cs
+++ TestLogicPuns/LogicPunsTester.cs
@@ -1,5 +1,6 @@
 ﻿using Codility;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineLogicPuns;
 
 namespace TestLogicPuns
 {
@@ -17,6 +18,17 @@ namespace TestLogicPuns
             int[][] expected = {first, second, resultThird};
             MatZeroDistances matZeroDistances = new MatZeroDistances();
             Assert.AreEqual(matZeroDistances.UpdateMatrix(input), expected);
+        }
+
+        [TestMethod]
+        public void TestSpiralMatrixCalculation()
+        {
+            const int numberOfColumns = 4;
+            const int numberOfRows = 4;
+            int[,] valueArray = new int[numberOfRows, numberOfColumns] { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
+
+            SpiralMatrixCalculation spiralMatrixCalculation = new SpiralMatrixCalculation();
+            spiralMatrixCalculation.CalculateMatrixSum(valueArray, numberOfColumns, numberOfRows);
         }
     }
 }
